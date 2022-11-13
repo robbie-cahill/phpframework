74dc6f705027a3916d4fe1b66bc533a812ad053c
Robbie Cahill <insprintorob@users.noreply.github.com>
2022-11-13 08:54:14 +0800

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+// This will make referencing files by path much simpler
+$rootDir = realpath(__DIR__ . '/..');
+define('ROOT_DIR', $rootDir);
+
+require ROOT_DIR . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->run();
