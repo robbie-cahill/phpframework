a892945c713a8745eaa60cd1e2a60e224e81309b
Robbie Cahill <insprintorob@users.noreply.github.com>
2022-12-02 16:19:56 +0800

    Add comment for XSS

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -15,6 +15,9 @@ class HelloController
     public function hello(Request $request, Response $response, array $args) 
     {
         $name = $args['name'];
+        
+        // In production, be sure to filter the input ($name) or escape the output to prevent XSS
+        // How you do that is up to you, but you could use for example: https://www.php.net/manual/en/function.filter-var.php
         $response->getBody()->write($this->greeter->greet($name));
         return $response;
     }
